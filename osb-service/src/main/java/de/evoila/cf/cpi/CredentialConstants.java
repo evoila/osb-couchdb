20f36a51fe03d85fb4fab828cf184bc12099346d
Marco Di Martino <marco.dima.93@gmail.com>
2019-11-22 14:07:00 +0100

    feat(): move to Credential Store

---
+++ osb-service/src/main/java/de/evoila/cf/cpi/CredentialConstants.java
@@ -0,0 +1,10 @@
+package de.evoila.cf.cpi;
+
+/**
+ * @author Johannes Hiemer.
+ */
+public class CredentialConstants {
+
+    public static String ROOT_CREDENTIALS = "root_admin";
+
+}
--- osb-service/src/main/java/de/evoila/cf/cpi/bosh/CouchDBDeploymentManager.java
+++ osb-service/src/main/java/de/evoila/cf/cpi/bosh/CouchDBDeploymentManager.java
@@ -3,8 +3,11 @@ package de.evoila.cf.cpi.bosh;
 import de.evoila.cf.broker.bean.BoshProperties;
 import de.evoila.cf.broker.model.ServiceInstance;
 import de.evoila.cf.broker.model.catalog.plan.Plan;
+import de.evoila.cf.broker.model.credential.UsernamePasswordCredential;
 import de.evoila.cf.cpi.bosh.deployment.DeploymentManager;
+import de.evoila.cf.cpi.CredentialConstants;
 import de.evoila.cf.cpi.bosh.deployment.manifest.Manifest;
+import de.evoila.cf.security.credentials.CredentialStore;
 import de.evoila.cf.security.utils.RandomString;
 import org.springframework.core.env.Environment;
 
@@ -19,9 +22,11 @@ public class CouchDBDeploymentManager extends DeploymentManager {
 
     private RandomString randomStringPassword = new RandomString(15);
 
+    private CredentialStore credentialStore;
 
-    public CouchDBDeploymentManager(BoshProperties boshProperties, Environment environment) {
+    public CouchDBDeploymentManager(BoshProperties boshProperties, Environment environment, CredentialStore credentialStore) {
         super(boshProperties, environment);
+        this.credentialStore = credentialStore;
     }
 
     @Override
@@ -37,17 +42,17 @@ public class CouchDBDeploymentManager extends DeploymentManager {
                 .filter(i -> i.getName().equals(INSTANCE_GROUP))
                 .findAny().get().getProperties();
 
-        String username = randomStringUsername.nextString();
-        String password = randomStringPassword.nextString();
+        UsernamePasswordCredential rootCredentials = credentialStore.createUser(serviceInstance,
+                CredentialConstants.ROOT_CREDENTIALS, "root");
 
         Map<String, Object> couchdb = (Map<String, Object>) manifestProperties.get("couchdb");
         Map<String, Object> auth = (Map<String, Object>) couchdb.get("auth");
 
-        auth.put("username", username);
-        auth.put("password", password);
+        auth.put("username", rootCredentials.getUsername());
+        auth.put("password", rootCredentials.getPassword());
 
-        serviceInstance.setUsername(username);
-        serviceInstance.setPassword(password);
+        serviceInstance.setUsername(rootCredentials.getUsername());
+        serviceInstance.setPassword(rootCredentials.getPassword());
 
         this.updateInstanceGroupConfiguration(manifest, plan);
     }
